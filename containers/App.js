908233354aa2c84d4f51a31e35407ce10d61f0d0
Pencil <9807175@qq.com>
2017-08-26 10:38:13 +0800

    Using prop-types instead of React.PropTypes

--- containers/App.js
+++ containers/App.js
@@ -1,4 +1,4 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import Game from '../components/Game'
 import NewGame from '../components/NewGame'
